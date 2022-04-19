5bfbc3124e01660846230ab9598c66bed687fad7
Mohammed Al-Asfar <elasfarc@gmail.com>
2022-04-19 05:19:06 +0200

    Add infinite scroll for SW-persons

--- base-infinite-swapi/src/App.jsx
+++ base-infinite-swapi/src/App.jsx
@@ -1,14 +1,21 @@
 import "./App.css";
 import { InfinitePeople } from "./people/InfinitePeople";
 import { InfiniteSpecies } from "./species/InfiniteSpecies";
+import { QueryClientProvider, QueryClient } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+
+const client = new QueryClient();
 
 function App() {
   return (
-    <div className="App">
-      <h1>Infinite SWAPI</h1>
-      <InfinitePeople />
-      {/* <InfiniteSpecies /> */}
-    </div>
+    <QueryClientProvider client={client}>
+      <div className="App">
+        <h1>Infinite SWAPI</h1>
+        <InfinitePeople />
+        {/* <InfiniteSpecies /> */}
+      </div>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
 
--- base-infinite-swapi/src/people/InfinitePeople.jsx
+++ base-infinite-swapi/src/people/InfinitePeople.jsx
@@ -1,13 +1,36 @@
 import InfiniteScroll from "react-infinite-scroller";
 import { Person } from "./Person";
+import { useInfiniteQuery } from "react-query";
 
-const initialUrl = "https://swapi.dev/api/people/";
-const fetchUrl = async (url) => {
-  const response = await fetch(url);
+const initialUrl = `https://swapi.dev/api/people/?page=`;
+const fetchUrl = async (pageN) => {
+  const response = await fetch(`${initialUrl}${pageN}`);
   return response.json();
 };
 
 export function InfinitePeople() {
-  // TODO: get data for InfiniteScroll via React Query
-  return <InfiniteScroll />;
+  const { data, fetchNextPage, hasNextPage, isLoading, isFetchingNextPage } =
+    useInfiniteQuery("sw-persons", ({ pageParam = 1 }) => fetchUrl(pageParam), {
+      getNextPageParam: (lastPage, allPages) => {
+        return allPages.length < Math.ceil(82 / 10)
+          ? allPages.length + 1
+          : null;
+      },
+    });
+  if (isLoading) return <div className="loading">Loading...</div>;
+  return (
+    <InfiniteScroll loadMore={fetchNextPage} hasMore={hasNextPage}>
+      {isFetchingNextPage ? <div className="loading">Loading...</div> : null}
+      {data.pages.map((pageData) => {
+        return pageData.results.map((person) => (
+          <Person
+            key={person.name}
+            name={person.name}
+            hairColor={person.hair_color}
+            eyeColor={person.eye_color}
+          ></Person>
+        ));
+      })}
+    </InfiniteScroll>
+  );
 }
